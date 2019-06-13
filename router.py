f74be35a96aa1bbe7d8ca4c55194fcc3211ed5f8
Marek M <mamolli@Mareks-MacBook-Pro.local>
2019-06-13 22:59:44 +0200

    flask like routing

--- router.py
+++ router.py
@@ -1,3 +1,5 @@
+import re
+
 REGISTRY = []
 
 # avoid OOP at all costs
@@ -5,6 +7,9 @@ def match(regex):
     def regex_deco(action):
         def fu(message=None, user=None):
             return action(message, user)
+        # validate regex
+        if validate_regex(regex):
+            REGISTRY.append(regex, fu)
         return fu
     return regex_deco
 
@@ -13,4 +18,15 @@ def out(message, user):
     print("out")
     return 1
 
-print(out(2, 3))
+def validate_regex(regex):
+    try:
+        re.compile(regex)
+        valid = True
+    except re.error:
+        valid = False
+    return valid
+
+def route(message, user):
+    for action in REGISTRY:
+
+print(REGISTRY[0]())
