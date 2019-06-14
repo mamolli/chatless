1deffb0146f4a2f76367a13ce115781cb93f47b5
Marek M <mamolli@Mareks-MacBook-Pro.local>
2019-06-14 23:35:50 +0200

    tf gateway

--- main.tf
+++ main.tf
@@ -13,7 +13,61 @@ resource "aws_lambda_function" "lambda_function" {
 resource "aws_iam_role" "lambda_exec_role" {
   name        = "lambda_exec"
   path        = "/"
-  description = "Allows Lambda Function to call AWS services on your behalf."
+  description = ""
 
   assume_role_policy = "${file("lambda_policy.json")}"
+}
+
+resource "aws_api_gateway_rest_api" "lambda_chatbot" {
+  name        = "lambda_chatbot"
+  description = ""
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = "${aws_api_gateway_rest_api.lambda_chatbot.id}"
+  parent_id   = "${aws_api_gateway_rest_api.lambda_chatbot.root_resource_id}"
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = "${aws_api_gateway_rest_api.lambda_chatbot.id}"
+  resource_id   = "${aws_api_gateway_resource.proxy.id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.lambda_chatbot.id}"
+  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.lambda_function.invoke_arn}"
+}
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = "${aws_api_gateway_rest_api.lambda_chatbot.id}"
+  resource_id   = "${aws_api_gateway_rest_api.lambda_chatbot.root_resource_id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = "${aws_api_gateway_rest_api.lambda_chatbot.id}"
+  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.lambda_function.invoke_arn}"
+}
+
+resource "aws_api_gateway_deployment" "lambda" {
+  depends_on = [
+    "aws_api_gateway_integration.lambda",
+    "aws_api_gateway_integration.lambda_root",
+  ]
+
+  rest_api_id = "${aws_api_gateway_rest_api.lambda_chatbot.id}"
+  stage_name  = "test"
 }
