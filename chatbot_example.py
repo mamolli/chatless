979634759ec71f41be4f920f730ce9047b68cb91
mamolli <roguas@gmail.com>
2019-12-22 13:56:44 +0100

    removing unwanted deps

--- chatbot_example.py
+++ chatbot_example.py
@@ -3,7 +3,8 @@ import chatless
 @chatless.default
 @chatless.match(r"/hi|hello|man|help|docs/")
 def show_help(bot_event):
-    return "hi man"
+    return "help shown here"
+
 
 def handle(event, context):
     return chatless.handle(event)
